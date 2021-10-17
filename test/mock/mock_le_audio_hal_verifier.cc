protonaosp-gsi/android_system_bt

commit 280b895ce020356ec9862f24de3dacea99bdf813

leaudio: Add BAP and CSIP integration along with audio path
LE Audio implements PAC handling, device grouping into sets,
audio policy by the content type, group stream management,
broadcast etc.
Devices are always part of the group which creates sets.
Sets can be managed by the LeAudio system service which has an API to add/remove
specific devices to/from the group.

Any stream operations are done by the LeAudio system service
with a usage of the group_id.

Note, this group_id is not the same number as the CIG_ID however,
we might say it has the same meaning.

When devices are part of a coordinated set (CSIS), then LeAudio
implementation will discover all of them and notify upper layer about them.
Upper layers are to decide to bond CSIS devices. Native code will
group them.

Fluoride also integrates Android Audio 2.1 framework and once stream is
established then Le Audio session is opened and ready to receive
periodically (up to implementation) audio data from media server
(16bit, 16kHz/24kHz/32kHz/48kHz PCM - up to policy implementation) from
the audio system.

PCM frames are encoded by LC3 codec (system/bt/embdrv/lc3) and pushed over HCI to the
controller.

Bug: 150670922
Tag: #feature
Sponsor: jpawlowski@
Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test

Change-Id: I25d9422fb2b43843a59a9e398fac1e09c7e4869c

bta/Android.bp
@@ -91,6 +91,12 @@ cc_library_static {
         "groups/groups.cc",
         "vc/device.cc",
         "vc/vc.cc",
+        "le_audio/client.cc",
+        "le_audio/devices.cc",
+        "le_audio/state_machine.cc",
+        "le_audio/client_parser.cc",
+        "le_audio/client_audio.cc",
+        "le_audio/le_audio_types.cc",
         "hearing_aid/hearing_aid.cc",
         "hearing_aid/hearing_aid_audio_source.cc",
         "hf_client/bta_hf_client_act.cc",
@@ -394,3 +400,114 @@ cc_test {
         cfi: false,
     },
 }
+
+// bta unit tests for LE Audio
+// ========================================================
+cc_test {
+    name: "bluetooth_le_audio_test",
+    test_suites: ["device-tests"],
+    defaults: [
+        "fluoride_defaults",
+        "clang_coverage_bin",
+    ],
+    host_supported: true,
+    include_dirs: [
+        "system/bt",
+        "system/bt/bta/include",
+        "system/bt/bta/test/common",
+        "system/bt/btif/include",
+        "system/bt/gd",
+        "system/bt/stack/include",
+    ],
+    srcs : [
+        ":TestStubOsi",
+        "test/common/bta_gatt_api_mock.cc",
+        "test/common/bta_gatt_queue_mock.cc",
+        "test/common/btm_api_mock.cc",
+        "le_audio/client_audio.cc",
+        "le_audio/client_audio_test.cc",
+        "le_audio/client_parser.cc",
+        "le_audio/client_parser_test.cc",
+        "le_audio/devices.cc",
+        "le_audio/devices_test.cc",
+        "le_audio/le_audio_types.cc",
+        "le_audio/le_audio_types_test.cc",
+        "le_audio/mock_iso_manager.cc",
+        "test/common/mock_controller.cc",
+        "le_audio/state_machine.cc",
+        "le_audio/state_machine_test.cc"
+    ],
+    shared_libs: [
+        "libprotobuf-cpp-lite",
+        "libcrypto",
+        "liblog",  // __android_log_print
+    ],
+    static_libs : [
+        "libgmock",
+        "libbt-common",
+        "libbt-protos-lite",
+        "libosi",
+    ],
+    sanitize: {
+        cfi: false,
+    },
+}
+
+cc_test {
+    name: "bluetooth_le_audio_client_test",
+    test_suites: ["device-tests"],
+    defaults: [
+        "fluoride_bta_defaults",
+        "clang_coverage_bin",
+    ],
+    host_supported: true,
+    include_dirs: [
+        "system/bt",
+        "system/bt/bta/include",
+        "system/bt/bta/test/common",
+        "system/bt/stack/include",
+    ],
+    srcs : [
+        "gatt/database.cc",
+        "gatt/database_builder.cc",
+        "le_audio/client.cc",
+        "le_audio/client_parser.cc",
+        "le_audio/devices.cc",
+        "le_audio/le_audio_client_test.cc",
+        "le_audio/le_audio_types.cc",
+        "le_audio/mock_iso_manager.cc",
+        "le_audio/mock_le_audio_client_audio.cc",
+        "le_audio/mock_state_machine.cc",
+        "test/common/btm_api_mock.cc",
+        "test/common/bta_gatt_api_mock.cc",
+        "test/common/bta_gatt_queue_mock.cc",
+        "test/common/btif_storage_mock.cc",
+        "test/common/mock_csis_client.cc",
+        "test/common/mock_controller.cc",
+        "test/common/mock_device_groups.cc",
+    ],
+    shared_libs: [
+        "libprotobuf-cpp-lite",
+        "libcrypto",
+        "liblog",
+    ],
+    static_libs : [
+        "crypto_toolbox_for_tests",
+        "libgmock",
+        "libbt-common",
+        "libbt-protos-lite",
+        "libosi",
+        "liblc3codec",
+    ],
+    sanitize: {
+        cfi: true,
+        scs: true,
+        address: true,
+        all_undefined: true,
+        integer_overflow: true,
+        diag: {
+            undefined : true
+        },
+    },
+}
+

bta/include/bta_le_audio_api.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright 2019 HIMSA II K/S - www.himsa.com. Represented by EHIMA -
+ * www.ehima.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <base/callback_forward.h>
+#include <hardware/bt_le_audio.h>
+
+/* Interface class */
+class LeAudioClient {
+ public:
+  virtual ~LeAudioClient(void) = default;
+
+  static void Initialize(bluetooth::le_audio::LeAudioClientCallbacks* callbacks,
+                         base::Closure initCb,
+                         base::Callback<bool()> hal_2_1_verifier);
+  static void Cleanup(void);
+  static LeAudioClient* Get(void);
+  static void DebugDump(int fd);
+
+  virtual void RemoveDevice(const RawAddress& address) = 0;
+  virtual void Connect(const RawAddress& address) = 0;
+  virtual void Disconnect(const RawAddress& address) = 0;
+  virtual void GroupAddNode(const int group_id, const RawAddress& addr) = 0;
+  virtual void GroupRemoveNode(const int group_id, const RawAddress& addr) = 0;
+  virtual void GroupStream(const int group_id, const uint16_t content_type) = 0;
+  virtual void GroupSuspend(const int group_id) = 0;
+  virtual void GroupStop(const int group_id) = 0;
+  virtual void GroupDestroy(const int group_id) = 0;
+  virtual void GroupSetActive(const int group_id) = 0;
+  virtual std::vector<RawAddress> GetGroupDevices(const int group_id) = 0;
+  static void AddFromStorage(const RawAddress& addr, bool autoconnect);
+  static bool IsLeAudioClientRunning();
+};

btif/Android.bp
@@ -178,7 +178,10 @@ cc_library_static {
             srcs: ["src/btif_avrcp_audio_track.cc"],
         },
         host: {
-            srcs: ["src/btif_avrcp_audio_track_linux.cc"],
+            srcs: [
+                     "src/btif_avrcp_audio_track_linux.cc",
+                     "src/btif_leaudio_hal_version_host.cc",
+                  ],
         },
     },
     whole_static_libs: [
@@ -228,6 +231,7 @@ cc_test {
         "libbt-utils",
         "libFraunhoferAAC",
         "libg722codec",
+        "liblc3codec",
         "libbtdevice",
         "libbt-hci",
         "libudrv-uipc",
@@ -405,6 +409,7 @@ cc_test {
           ":TestMockBtaHf",
           ":TestMockBtaHh",
           ":TestMockBtaJv",
+          ":TestMockBtaLeAudio",
           ":TestMockBtaPan",
           ":TestMockBtaSdp",
           ":TestMockBtaSys",

btif/src/btif_le_audio.cc
@@ -20,10 +20,11 @@
 
 #include <vector>
 
+#include "audio_hal_interface/hal_version_manager.h"
+#include "bta_le_audio_api.h"
 #include "btif_common.h"
 #include "btif_storage.h"
-#include "hardware/bt_le_audio.h"
-#include "types/raw_address.h"
+#include "stack/include/btu.h"
 
 using base::Bind;
 using base::Unretained;
@@ -70,21 +71,69 @@ class LeAudioClientInterfaceImpl : public LeAudioClientInterface,
 
   void Initialize(LeAudioClientCallbacks* callbacks) override {
     this->callbacks = callbacks;
+    do_in_main_thread(
+        FROM_HERE,
+        Bind(&LeAudioClient::Initialize, this,
+             jni_thread_wrapper(FROM_HERE,
+                                Bind(&btif_storage_load_bonded_leaudio)),
+             base::Bind([]() -> bool {
+               return bluetooth::audio::HalVersionManager::GetHalVersion() ==
+                      bluetooth::audio::BluetoothAudioHalVersion::VERSION_2_1;
+             })));
   }
 
-  void Cleanup(void) override {}
+  void Cleanup(void) override {
+    DVLOG(2) << __func__;
+    do_in_main_thread(FROM_HERE, Bind(&LeAudioClient::Cleanup));
+  }
 
-  void RemoveDevice(const RawAddress& address) override {}
+  void RemoveDevice(const RawAddress& address) override {
+    DVLOG(2) << __func__ << " address: " << address;
+    do_in_main_thread(FROM_HERE,
+                      Bind(&LeAudioClient::RemoveDevice,
+                           Unretained(LeAudioClient::Get()), address));
 
-  void Connect(const RawAddress& address) override {}
+    do_in_jni_thread(FROM_HERE, Bind(&btif_storage_remove_leaudio, address));
+  }
 
-  void GroupAddNode(const int group_id, const RawAddress& addr) override {}
+  void Connect(const RawAddress& address) override {
+    DVLOG(2) << __func__ << " address: " << address;
+    do_in_main_thread(FROM_HERE,
+                      Bind(&LeAudioClient::Connect,
+                           Unretained(LeAudioClient::Get()), address));
+  }
 
-  void Disconnect(const RawAddress& address) override {}
+  void Disconnect(const RawAddress& address) override {
+    DVLOG(2) << __func__ << " address: " << address;
+    do_in_main_thread(FROM_HERE,
+                      Bind(&LeAudioClient::Disconnect,
+                           Unretained(LeAudioClient::Get()), address));
+    do_in_jni_thread(
+        FROM_HERE, Bind(&btif_storage_set_leaudio_autoconnect, address, false));
+  }
 
-  void GroupRemoveNode(const int group_id, const RawAddress& addr) override {}
+  void GroupAddNode(const int group_id, const RawAddress& address) override {
+    DVLOG(2) << __func__ << " group_id: " << group_id
+             << " address: " << address;
+    do_in_main_thread(
+        FROM_HERE, Bind(&LeAudioClient::GroupAddNode,
+                        Unretained(LeAudioClient::Get()), group_id, address));
+  }
 
-  void GroupSetActive(const int group_id) override {}
+  void GroupRemoveNode(const int group_id, const RawAddress& address) override {
+    DVLOG(2) << __func__ << " group_id: " << group_id
+             << " address: " << address;
+    do_in_main_thread(
+        FROM_HERE, Bind(&LeAudioClient::GroupRemoveNode,
+                        Unretained(LeAudioClient::Get()), group_id, address));
+  }
+
+  void GroupSetActive(const int group_id) override {
+    DVLOG(2) << __func__ << " group_id: " << group_id;
+    do_in_main_thread(FROM_HERE,
+                      Bind(&LeAudioClient::GroupSetActive,
+                           Unretained(LeAudioClient::Get()), group_id));
+  }
 
  private:
   LeAudioClientCallbacks* callbacks;

test/Android.bp
@@ -97,6 +97,13 @@ filegroup {
 }
 
 filegroup {
+    name: "TestMockBtaLeAudio",
+    srcs: [
+      "mock/mock_bta_leaudio*.cc",
+    ],
+}
+
+filegroup {
     name: "TestMockBtaPan",
     srcs: [
       "mock/mock_bta_pan_*.cc",
@@ -139,6 +146,7 @@ filegroup {
       ":TestMockBtaHf",
       ":TestMockBtaHh",
       ":TestMockBtaJv",
+      ":TestMockBtaLeAudio",
       ":TestMockBtaPan",
       ":TestMockBtaSdp",
       ":TestMockBtaSys",

embdrv/lc3/Android.bp
@@ -33,13 +33,17 @@ cc_library_static {
         "-Wno-self-assign",
         "-Wno-implicit-fallthrough",
     ],
-    sanitize: {
-        misc_undefined:[
-           "unsigned-integer-overflow",
-           "signed-integer-overflow",
-           "bounds",
-        ],
-        cfi: true,
+    target: {
+       android: {
+            sanitize: {
+                misc_undefined:[
+                   "unsigned-integer-overflow",
+                   "signed-integer-overflow",
+                   "bounds",
+                ],
+                cfi: true,
+            },
+       },
     },
     shared_libs: [
         "liblog",

gd/rust/topshim/facade/Android.bp
@@ -43,6 +43,7 @@ rust_binary_host {
         "libbt-sbc-encoder",
         "libFraunhoferAAC",
         "libg722codec",
+        "liblc3codec",
         "libudrv-uipc",
         "libbluetooth_gd", // Gabeldorsche
         "libbluetooth_rust_interop",

commit a8bc6e1c3dfefcf3d7156423b6b2f765941394a0

leaudio: Fix build on Linux
This fixes the build issues on Linux introduced by
(280b895ce020: leaudio: Add BAP and CSIP integration...)

Bug: 150670922
Bug: 203206209
Tag: #floss
Test: ./build.py on Linux and mma -j "system/bt" on Android
Change-Id: I5b146d301d919780791d354720ae74a88c5b60ef

bta/Android.bp
@@ -93,6 +93,7 @@ cc_library_static {
         "vc/vc.cc",
         "le_audio/client.cc",
         "le_audio/devices.cc",
+        "le_audio/hal_verifier.cc",
         "le_audio/state_machine.cc",
         "le_audio/client_parser.cc",
         "le_audio/client_audio.cc",
@@ -133,6 +134,10 @@ cc_library_static {
     static_libs: [
         "avrcp-target-service",
         "lib-bt-packets",
+    ],
+    shared_libs: [
+        "android.hardware.bluetooth.audio@2.0",
+        "android.hardware.bluetooth.audio@2.1",
     ],
     host_supported: true,
 }
@@ -468,6 +473,7 @@ cc_test {
         "system/bt/stack/include",
     ],
     srcs : [
+        ":TestMockBtaLeAudioHalVerifier",
         "gatt/database.cc",
         "gatt/database_builder.cc",
         "le_audio/client.cc",
@@ -487,6 +493,8 @@ cc_test {
         "test/common/mock_device_groups.cc",
     ],
     shared_libs: [
+        "android.hardware.bluetooth.audio@2.0",
+        "android.hardware.bluetooth.audio@2.1",
         "libprotobuf-cpp-lite",
         "libcrypto",
         "liblog",

bta/BUILD.gn
@@ -74,6 +74,8 @@ static_library("bta") {
     "jv/bta_jv_act.cc",
     "jv/bta_jv_api.cc",
     "jv/bta_jv_cfg.cc",
+    "le_audio/client_linux.cc",
+    "le_audio/hal_verifier_linux.cc",
     "pan/bta_pan_act.cc",
     "pan/bta_pan_api.cc",
     "pan/bta_pan_ci.cc",

bta/include/bta_le_audio_api.h
@@ -17,8 +17,17 @@
 
 #pragma once
 
+#include <base/bind.h>
+#include <base/callback.h>
 #include <base/callback_forward.h>
 #include <hardware/bt_le_audio.h>
+
+#include <vector>
+
+class LeAudioHalVerifier {
+ public:
+  static bool SupportsLeAudio();
+};
 
 /* Interface class */
 class LeAudioClient {

bta/le_audio/client_linux.cc
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bta_le_audio_api.h"
+
+class LeAudioClientImpl : public LeAudioClient {
+ public:
+  LeAudioClientImpl(void) = default;
+  ~LeAudioClientImpl(void) override = default;
+
+  void RemoveDevice(const RawAddress& address) override {}
+  void Connect(const RawAddress& address) override {}
+  void Disconnect(const RawAddress& address) override {}
+  void GroupAddNode(const int group_id, const RawAddress& addr) override {}
+  void GroupRemoveNode(const int group_id, const RawAddress& addr) override {}
+  void GroupStream(const int group_id, const uint16_t content_type) override {}
+  void GroupSuspend(const int group_id) override {}
+  void GroupStop(const int group_id) override {}
+  void GroupDestroy(const int group_id) override {}
+  void GroupSetActive(const int group_id) override {}
+  std::vector<RawAddress> GetGroupDevices(const int group_id) override {
+    return {};
+  }
+};
+
+void LeAudioClient::Initialize(
+    bluetooth::le_audio::LeAudioClientCallbacks* callbacks,
+    base::Closure initCb, base::Callback<bool()> hal_2_1_verifier) {}
+void LeAudioClient::Cleanup(void) {}
+LeAudioClient* LeAudioClient::Get(void) { return nullptr; }
+void LeAudioClient::DebugDump(int fd) {}
+void LeAudioClient::AddFromStorage(const RawAddress& addr, bool autoconnect) {}
+bool LeAudioClient::IsLeAudioClientRunning() { return false; }

bta/le_audio/hal_verifier.cc
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "audio_hal_interface/hal_version_manager.h"
+#include "bta_le_audio_api.h"
+
+bool LeAudioHalVerifier::SupportsLeAudio() {
+  return bluetooth::audio::HalVersionManager::GetHalVersion() ==
+         bluetooth::audio::BluetoothAudioHalVersion::VERSION_2_1;
+}

bta/le_audio/hal_verifier_linux.cc
@@ -1,0 +1,19 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bta_le_audio_api.h"
+
+bool LeAudioHalVerifier::SupportsLeAudio() { return false; }

btif/Android.bp
@@ -410,6 +410,7 @@ cc_test {
           ":TestMockBtaHh",
           ":TestMockBtaJv",
           ":TestMockBtaLeAudio",
+          ":TestMockBtaLeAudioHalVerifier",
           ":TestMockBtaPan",
           ":TestMockBtaSdp",
           ":TestMockBtaSys",

btif/src/btif_le_audio.cc
@@ -20,7 +20,6 @@
 
 #include <vector>
 
-#include "audio_hal_interface/hal_version_manager.h"
 #include "bta_le_audio_api.h"
 #include "btif_common.h"
 #include "btif_storage.h"
@@ -72,14 +71,12 @@ class LeAudioClientInterfaceImpl : public LeAudioClientInterface,
   void Initialize(LeAudioClientCallbacks* callbacks) override {
     this->callbacks = callbacks;
     do_in_main_thread(
-        FROM_HERE,
-        Bind(&LeAudioClient::Initialize, this,
-             jni_thread_wrapper(FROM_HERE,
-                                Bind(&btif_storage_load_bonded_leaudio)),
-             base::Bind([]() -> bool {
-               return bluetooth::audio::HalVersionManager::GetHalVersion() ==
-                      bluetooth::audio::BluetoothAudioHalVersion::VERSION_2_1;
-             })));
+        FROM_HERE, Bind(&LeAudioClient::Initialize, this,
+                        jni_thread_wrapper(
+                            FROM_HERE, Bind(&btif_storage_load_bonded_leaudio)),
+                        base::Bind([]() -> bool {
+                          return LeAudioHalVerifier::SupportsLeAudio();
+                        })));
   }
 
   void Cleanup(void) override {

bta/le_audio/le_audio_client_test.cc
@@ -59,6 +59,8 @@ using bluetooth::Uuid;
 
 using namespace bluetooth::le_audio;
 
+std::map<std::string, int> mock_function_count_map;
+
 // Disables most likely false-positives from base::SplitString()
 extern "C" const char* __asan_default_options() {
   return "detect_container_overflow=0";

test/Android.bp
@@ -104,6 +104,13 @@ filegroup {
 }
 
 filegroup {
+  name: "TestMockBtaLeAudioHalVerifier",
+  srcs: [
+      "mock/mock_le_audio_hal_verifier.cc",
+  ],
+}
+
+filegroup {
     name: "TestMockBtaPan",
     srcs: [
       "mock/mock_bta_pan_*.cc",
@@ -147,6 +154,7 @@ filegroup {
       ":TestMockBtaHh",
       ":TestMockBtaJv",
       ":TestMockBtaLeAudio",
+      ":TestMockBtaLeAudioHalVerifier",
       ":TestMockBtaPan",
       ":TestMockBtaSdp",
       ":TestMockBtaSys",

test/mock/mock_le_audio_hal_verifier.cc
@@ -1,0 +1,26 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <map>
+#include <string>
+
+extern std::map<std::string, int> mock_function_count_map;
+
+#include "bta/include/bta_le_audio_api.h"
+
+bool LeAudioHalVerifier::SupportsLeAudio() {
+  mock_function_count_map[__func__]++;
+  return true;
+}